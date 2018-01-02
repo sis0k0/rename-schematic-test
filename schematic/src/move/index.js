4ddb9e7a838d0758a9971410dd7bf964f8e5af3d
sis0k0 <stanimira.vlaeva@gmail.com>
2018-01-02 17:58:23 +0200

    refactor: move plugin to schematics/

---
+++ schematic/src/move/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const schematics_1 = require("@angular-devkit/schematics");
+function default_1(_options) {
+    return schematics_1.move('dir-with-subdirs', 'new-dir');
+}
+exports.default = default_1;
+//# sourceMappingURL=index.js.map
---
+++ schematic/src/rename/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function default_1(_options) {
+    return (tree) => {
+        tree.rename('dir1/1', 'dir2/1');
+    };
+}
+exports.default = default_1;
+//# sourceMappingURL=index.js.map
