ca92e94671e332951f55707fec8a5c4dd656c87c
sis0k0 <stanimira.vlaeva@gmail.com>
2018-01-02 18:31:25 +0200

    refactor: remove .js and .js.map files

--- schematic/src/move/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const schematics_1 = require("@angular-devkit/schematics");
-function default_1(_options) {
-    return schematics_1.move('dir-with-subdirs', 'new-dir');
-}
-exports.default = default_1;
-//# sourceMappingURL=index.js.map
--- schematic/src/rename/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(_options) {
-    return (tree) => {
-        tree.rename('dir1/1', 'dir2/1');
-    };
-}
-exports.default = default_1;
-//# sourceMappingURL=index.js.map
